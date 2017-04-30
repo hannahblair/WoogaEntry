e4064c901c58cefd9472e794aa6a81af87d9a235
Hannah Blair <hannahblair@users.noreply.github.com>
2017-04-30 23:58:00 +0100

    Add files via upload

---
+++ Scripts/RandomSprite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSprite : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public int CurrentSprite = -1;
+
+	// Use this for initialization
+	void Start () {
+
+        if(CurrentSprite == -1)
+        {
+            CurrentSprite = Random.Range(0, sprites.Length);
+        } else if (CurrentSprite > sprites.Length)
+        {
+            CurrentSprite = sprites.Length - 1;
+        }
+        GetComponent<SpriteRenderer>().sprite = sprites[CurrentSprite];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
